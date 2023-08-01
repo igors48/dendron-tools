b20c0e6a8f08e5dcc6dbf4176cd95b83a5d8067f
igors48 <nmds48@gmail.com>
2023-08-01 08:06:01 +0300

    #6 find stalled documents
    app class refactored
    
    todo inject render
    todo test infra
    todo cleanup classes
    todo logging

--- app/src/main/java/nmd/app/App.java
+++ app/src/main/java/nmd/app/App.java
@@ -5,6 +5,7 @@ import nmd.cli.parser.CommandLineParser;
 import nmd.command.factory.Command;
 import nmd.command.factory.Factory;
 import nmd.command.factory.FileSystem;
+import nmd.command.factory.Time;
 
 /**
  * @author Igor Usenko
@@ -12,8 +13,12 @@ import nmd.command.factory.FileSystem;
 final class App {
 
     public static void main(String[] args) {
+        new App().run(args, FileSystem.DEFAULT, System::currentTimeMillis);
+    }
+
+    void run(String[] args, FileSystem fileSystem, Time time) {
         val parameters = CommandLineParser.parse(args);
-        Factory factory = new Factory(FileSystem.DEFAULT, System::currentTimeMillis);
+        Factory factory = new Factory(fileSystem, time);
         Command command = factory.create(parameters);
         command.execute();
     }
